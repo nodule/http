7c592fb7ed47a26c0ac04ff11e55b8c7500ddaef
Rob Halff <rob.halff@gmail.com>
2014-02-09 18:28:42 +0100

    update

--- nodes/createServer/node.js
+++ nodes/createServer/node.js
@@ -1,9 +1,1 @@
-output = function(cb) {
-
-  var h = http.createServer(function(req, res) {
-    cb({ request: req, response: res });
-  });
-
-  cb({ http: h });
-
-};
+output.http = http.createServer(input.app);
