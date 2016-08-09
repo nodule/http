ee968ccf9f5049997661c421d4468907f6f58d2a
Rob Halff <rob.halff@gmail.com>
2016-08-09 19:44:49 +0200

    change api

--- nodes/createServer/node.js
+++ nodes/createServer/node.js
@@ -1,1 +1,16 @@
-output.http = $.create(http.createServer($.app));
+output = function () {
+  var _http = http.createServer(function httpRequest (req, res) {
+    cb({
+      out: {
+        $.create({
+          request: req,
+          response: res
+        })
+      }
+    });
+  });
+
+  cb({
+    http: $.create(_http);
+  });
+};
